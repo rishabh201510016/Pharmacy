e9f62afd615920baeeba5cf183b86e5b2ff23caa
“rishabh201510016” <mishrarishabh1281@gmail.com>
2023-08-16 15:53:45 +0530

    Code pushed

---
+++ index.js
@@ -0,0 +1,17 @@
+const port_btn = document.querySelector(".p-btns");
+const p_btn = document.querySelectorAll(".p-btn");
+const img_div = document.querySelectorAll(".img-ovelay");
+
+// port_btn.addEventListener("click", (e) => {
+
+// const mobile_nav = document.querySelector(".mobile-navbar-btn");
+// const nav_header = document.querySelector(".header");
+
+// const btn = () => {
+//     nav_header.classList.toggle("active");
+//     <a href="https://www.1mg.com/drugs-all-medicines"
+//     target="_blank"
+//     class="explore_btn" >Explore Now</a>
+// };
+
+mobile_nav.addEventListener("click", () => togglebtn());
